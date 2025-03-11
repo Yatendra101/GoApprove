76d7ed2949d33247c4f0683ff4d6995adab3eee9
Yatendra101 <yatendras0@gmail.com>
2025-03-11 18:13:25 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,12 +3,14 @@ import axios from 'axios';
 const API_URL = 'https://jobapprove-backend.onrender.com'; // Adjust this if your endpoint is different
 
 export const login = async (credentials) => {
-    const response = await axios.post(`${API_URL}/login`, credentials);
+    const response = await axios.post(`${API_URL}/api/auth/login`, credentials);
     return response.data;
 };
 
+
 // Add the signup function
 export const signup = async (userData) => {
-    const response = await axios.post(`${API_URL}/signup`, userData);
+    const response = await axios.post(`${API_URL}/api/auth/signup`, userData);
     return response.data;
 };
+
